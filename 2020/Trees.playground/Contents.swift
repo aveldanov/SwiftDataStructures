62a666c25c6be3e981175a8a4f8c8f8c8162915c
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-01 22:08:51 -0800

    iOS

--- 2020/Trees.playground/Contents.swift
+++ 2020/Trees.playground/Contents.swift
@@ -13,13 +13,93 @@ class TreeNode<T>{
     
 }
 
+// create a tree
+let beverages = TreeNode("Beverages")
 
-let beverages = TreeNode<String>("Beverages")
+let hot = TreeNode("Hot")
+let cold = TreeNode("Cold")
 
-let hotBev = TreeNode<String>("hotBev")
+let tea = TreeNode("Tea")
+let coffee = TreeNode("Coffee")
 
-let coldBev = TreeNode<String>("coldBev")
+
+let soda = TreeNode("Soda")
+let milk = TreeNode("Milk")
+beverages.add(hot)
+beverages.add(cold)
+
+hot.add(tea)
+hot.add(coffee)
+cold.add(soda)
+cold.add(milk)
 
 
-beverages.add(hotBev)
-beverages.add(coldBev)
+
+extension TreeNode{
+    
+    func forEachDepthFirst(_ visit:(TreeNode)->Void){
+        visit(self)
+        childeren.forEach{
+            $0.forEachDepthFirst(visit)
+        }
+        
+    }
+    
+    
+}
+
+
+
+
+
+
+beverages.forEachDepthFirst { print($0.value)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+//CLOSURES
+
+func calc( a: Int, b:Int, operation: (Int,Int)->Int)->Int{
+    
+    return operation(a,b)
+}
+
+
+
+
+
+func add(x:Int, y:Int)->Int{
+    
+    return x+y
+}
+
+
+func multiply(x:Int, y:Int)->Int{
+    
+    return x*y
+}
+
+// ----->
+calc(a: 3, b: 3){$0*$1}
+
+calc(a: 2, b: 3) { (x, y) -> Int in
+    x*y
+}
+*/
