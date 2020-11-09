a750b1f3e2796f43aa08fe063a4edc419148484a
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-08 18:26:10 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -1,28 +1,16 @@
 
-var arr = [44,5,7,8,33,55,77,99,102]
 
-func bubbleSort(arr:[Int])->[Int]{
-    
-    var arr = arr
-    
-    print(arr)
-    
-    for i in 0..<arr.count{
-        for j in 0..<arr.count{
-            if arr[i]<arr[j]{
-                //swap
-//                let temp = arr[i]
-//                arr[i] = arr[j]
-//                arr[j] = temp
-                arr.swapAt(i, j)
-                
-            }
-        }
-    }
-    
-    
-    return arr
+enum EdgeType{
+    case directed
+    case undirected
 }
 
 
-print(bubbleSort(arr: arr))
+
+struct Vertex<T>{
+    let data: T
+    let index: Int
+    
+    
+    
+}
