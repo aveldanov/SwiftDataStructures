5d162d82a18ccd78d5a0806ac4e18deda386f60c
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-10 12:47:02 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -1,16 +1,42 @@
-
-
-enum EdgeType{
-    case directed
-    case undirected
+class TreeNode<T>{
+    var left: TreeNode?
+    var right: TreeNode?
+    var value: T
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func treeTraversal(visit:(_ value:T)->(Void)){
+        visit(value)
+        
+        left?.treeTraversal(visit: visit)
+        right?.treeTraversal(visit: visit)
+        
+    }
+    
+    
 }
 
 
+/*
+ 1
+ 2 3
+    4 5
+ 
+ 
+ */
 
-struct Vertex<T>{
-    let data: T
-    let index: Int
-    
-    
-    
-}
+
+let one = TreeNode(1)
+let two = TreeNode(2)
+let three = TreeNode(3)
+let four = TreeNode(4)
+let five = TreeNode(5)
+
+
+
+one.left = two
+one.right = three
+three.left = four
+three.right = five
+
