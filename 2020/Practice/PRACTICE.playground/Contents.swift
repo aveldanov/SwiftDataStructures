9a79391aaa0852f1405a9000bc8d292533683899
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-13 15:36:11 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -1,24 +1,5 @@
-var arr = [66,44,33,22,77,1,9,17,35,32]
-
-
-func bubbleSort(arr: inout[Int])->[Int]{
+class Stack<T>{
     
-    for i in 0..<arr.count-1{
-        for j in 0..<arr.count-1 {
-            print(j)
-            print("arr[j]",arr[j])
-            print("arr[j+1]",arr[j+1])
-
-            if arr[j] > arr[j+1]{
-                arr.swapAt(j+1, j)
-            }
-            print(arr)
-
-            print("")
-        }
-    }
-    return arr
+    
+    
 }
-
-
-print(bubbleSort(arr: &arr))
