30228f5bf1148c05002b5fb6e0487118b471c820
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-13 15:48:26 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -1,5 +1,28 @@
-class Stack<T>{
+class Queue<T>{
     
-      
+    var queueStore = [T]()
+    
+    
+    func enqueue(_ value:T)->Bool{
+        
+        queueStore.append(value)
+        return true
+    }
+    
+    func dequeue()->T?{
+        
+        return queueStore.isEmpty ? nil : queueStore.removeLast()
+    }
+
     
 }
+
+let queue = Queue<Int>()
+
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+
+print(queue)
+
