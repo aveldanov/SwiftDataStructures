9775ff0b8809542897ea86c34afd7a8b0f6493e0
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-12 19:52:55 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -1,31 +1,3 @@
-class Stack<T>{
+class TreeNode<T>{
     
-    var array: [T] = []
-    
-    
-    func displayStack(){
-        
-        print(array.map{
-            "\($0)"
-        }.reversed().joined(separator: ","))
-    }
-    
-    func push(_ value:T){
-        array.append(value)
-    }
-    func pop()->T?{
-        return array.popLast()
-    }
 }
-
-
-let stack = Stack<Int>()
-
-
-stack.push(4)
-stack.push(5)
-stack.push(6)
-
-stack.displayStack()
-stack.pop()
-stack.displayStack()
