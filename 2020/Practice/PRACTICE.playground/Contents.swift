97f25d142f15a45f6358688986b5141dc8319984
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-13 18:28:30 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -2,10 +2,22 @@ class Queue<T>{
     
     var array = [T]()
    
+    func enqueue(_ value:T)->Bool{
+        array.append(value)
+        return true
+    }
     
-    
-    func 
-    
+    func dequeue()->T?{
+        return array.isEmpty ? nil : array.removeFirst()
+    }
 }
 
 
+let queue = Queue<Int>()
+
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.dequeue()
+
+print(queue.array)
