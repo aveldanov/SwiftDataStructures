202e4a240f054783b967eb34213265035da2880e
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-13 18:01:49 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -1,27 +1,59 @@
-class Queue<T>{
-    
-    var queueStore = [T]()
-    
-    
-    func enqueue(_ value:T)->Bool{
-        
-        queueStore.append(value)
-        return true
+class ListNode<T>{
+    var next: ListNode?
+    var  value: T
+    init(_ value: T, _ next: ListNode?) {
+        self.value = value
+        self.next = next
     }
-    
-    func dequeue()->T?{
-        
-        return queueStore.isEmpty ? nil : queueStore.removeFirst()    }
-
     
 }
 
-let queue = Queue<Int>()
 
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-queue.dequeue()
+class LinkedList<T>{
+    var head: ListNode<T>?
+    
+    func displayList(){
+        var current = head
+        
+        while current?.next != nil {
+            print(current?.value ?? "")
+            current = current?.next
+            
+        }
+    }
+    
+    
+    func push(_ value: T){
+        if head == nil{
+            head = ListNode(value, nil)
+        }
+        
+        var current = head
+        while current != nil {
+            current = current?.next
+        }
+        
+        head = ListNode(value, head)
+        
+        
+    }
+    
+    func insertAtIndex(_ value: T, _ index: Int){
+        if head == nil{
+            head = ListNode(value, nil)
+        }
+         var current = head
+        
+        
+    }
+    
+     
+}
 
-print(queue.queueStore)
+let linkedList = LinkedList<Int>()
 
+linkedList.push(3)
+linkedList.push(2)
+linkedList.push(1)
+
+linkedList.displayList()
