7362d0636b82eb9ae13643f8ad85dd7f4133b509
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-14 20:16:55 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -1,23 +1,58 @@
-class Queue<T>{
+struct Point{
+    var x: Int
+    var y: Int
     
-    var array = [T]()
-   
-    func enqueue(_ value:T)->Bool{
-        array.append(value)
-        return true
-    }
-    
-    func dequeue()->T?{
-        return array.isEmpty ? nil : array.removeFirst()
-    }
 }
 
 
-let queue = Queue<Int>()
+struct Size{
+    var width: Int
+    var height: Int
+}
 
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-queue.dequeue()
+public class ColumnLayout{
+    static func layoutChildViews(parentViewSize: Size, childViewSizes: [Size]) throws -> [Point]{
+                
+        var remainingWidth = parentViewSize.width
+        var remainingHeight = parentViewSize.height
+        var array = [Point]()
+        var point = Point(x: 0, y: 0)
+        var count = 0
+        for item in childViewSizes{
+            print("RemW", remainingWidth)
+            if item.width <= remainingWidth && count <= childViewSizes.count{
+                if item.height <= remainingHeight{
+                    point.y = parentViewSize.height - remainingHeight
+                    point.x = parentViewSize.width - remainingWidth
+                    print("point a ", point)
+                    array.append(point)
+                    remainingHeight -= item.height
 
-print(queue.array)
+                }else{
+                    remainingWidth -= item.width
+                    remainingHeight = parentViewSize.height
+                    point.y = parentViewSize.height - remainingHeight
+                    point.x = parentViewSize.width - remainingWidth
+                    print("point b",point)
+                    array.append(point)
+                    remainingHeight -= item.height
+
+                }
+            }else{
+               throw
+                return array
+            }
+
+        }
+        
+        
+        
+        return array
+    }
+    
+    
+}
+
+
+
+try ColumnLayout.layoutChildViews(parentViewSize: Size(width: 5, height: 10), childViewSizes: [Size(width: 2, height: 3), Size(width: 2, height: 3),Size(width: 2, height: 3),Size(width: 2, height: 3),Size(width: 2, height: 3),Size(width: 2, height: 3),Size(width: 2, height: 3)])
