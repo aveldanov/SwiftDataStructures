bffff748178cadf8a6bab33903d6453f6350b62f
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-17 10:12:26 -0800

    iOS

--- 2020/Practice/PRACTICE_PROJECT/PRACTICE_PROJECT/ViewController.swift
+++ 2020/Practice/PRACTICE_PROJECT/PRACTICE_PROJECT/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     let box: UIView = {
         let boxView = UIView()
         boxView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        boxView.layer.cornerRadius = 12
+        boxView.layer.cornerRadius = 25
         boxView.translatesAutoresizingMaskIntoConstraints = false
         boxView.clipsToBounds = true
         return boxView
@@ -25,12 +25,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(box)
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        setupLayout()
     }
  
+
     
-    @objc func handlePan(_ pan: UIPanGestureRecognizer){
-        if pan.state = 
+    func setupLayout(){
+        NSLayoutConstraint.activate([
+            box.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            box.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            box.widthAnchor.constraint(equalToConstant: 200),
+            box.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
     }
     
   
