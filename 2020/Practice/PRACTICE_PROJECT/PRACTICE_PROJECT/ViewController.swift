94a126b70d6851eacadad5de8e405f07a32a10cf
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-17 18:07:04 -0800

    iOS

--- 2020/Practice/PRACTICE_PROJECT/PRACTICE_PROJECT/ViewController.swift
+++ 2020/Practice/PRACTICE_PROJECT/PRACTICE_PROJECT/ViewController.swift
@@ -9,36 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     let box: UIView = {
-        let boxView = UIView()
-        boxView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        boxView.layer.cornerRadius = 25
-        boxView.translatesAutoresizingMaskIntoConstraints = false
-        boxView.clipsToBounds = true
-        return boxView
+        let box = UIView()
+        box.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        box.layer.cornerRadius = 12
+        box.translatesAutoresizingMaskIntoConstraints = false
+        box.clipsToBounds = true
+        return box
     }()
-    
-    var initialPoint = CGPoint()
+
+    var initialCenterPoint = CGPoint()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(box)
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(gestHandler(_:)))
         setupLayout()
+        
     }
  
-
+    
+    
+    @objc func gestHandler(_ pan: UIPanGestureRecognizer){
+        if pan.state == .began{
+            initialCenterPoint = box.center
+        }
+        
+        let translation = pan.translation(in: view)
+        
+        if pan.state != .cancelled{
+            let newCenter = CGPoint(x: initialCenterPoint.x + translation.x, y: initialCenterPoint.y + translation.y)
+            box.center = newCenter
+            
+        }else{
+            box.center = initialCenterPoint
+        }
+        
+    }
+    
     
     func setupLayout(){
         NSLayoutConstraint.activate([
+        
             box.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            box.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            box.widthAnchor.constraint(equalToConstant: 200),
-            box.heightAnchor.constraint(equalToConstant: 200)
+            box.centerYAnchor.constraint(equalTo: view.co)
         ])
         
     }
+
     
   
 }
