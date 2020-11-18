e8d1d9e1e7bf7f965cf527c3a0fe8d3629ebb07d
Anton Veldanov <anton.veldanov@gmail.com>
2020-11-17 19:06:49 -0800

    iOS

--- 2020/Practice/PRACTICE.playground/Contents.swift
+++ 2020/Practice/PRACTICE.playground/Contents.swift
@@ -1,1 +1,45 @@
 
+class Person{
+    var name: String
+    var laptop: Computer?
+    init(name:String, laptop: Computer?) {
+        self.name = name
+        self.laptop = laptop
+    }
+    
+    deinit {
+        print("\(name) has been deinit")
+    }
+}
+
+class Computer{
+    var laptopName: String
+    var owner: Person?
+    init(laptopName:String, owner: Person?) {
+        self.laptopName = laptopName
+        self.owner = owner
+    }
+    
+    deinit {
+        print("\(laptopName) has been deinit")
+
+    }
+}
+
+var anton: Person?
+var mac1: Computer?
+
+anton = Person(name: "anton", laptop: nil)
+mac1 = Computer(laptopName: "mac1L", owner: nil)
+
+
+func initComp(){
+    
+    anton?.laptop = mac1
+    mac1?.owner = anton
+    
+    anton = nil
+}
+
+
+initComp()
